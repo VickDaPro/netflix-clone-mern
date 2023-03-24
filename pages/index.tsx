7ae33aa1c65001f59ba6d8ec8266f069c2dbbc78
VickDaPro <vickbishnoi@gmail.com>
2023-03-25 00:13:31 +0530

    added login functionality and protected routes.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,37 @@
+import { signOut, getSession } from "next-auth/react";
+import { NextPageContext } from "next";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
-  return <>Hello</>;
+  const { data: user } = useCurrentUser();
+
+  return (
+    <>
+      <h1>Netflix Clone</h1>
+      <p className="text-4xl text-white font-bold">
+        Loggin in as :{user?.name}
+      </p>
+      <button onClick={() => signOut()} className="h-10 bg-white w-full">
+        Logout!
+      </button>
+    </>
+  );
 }
